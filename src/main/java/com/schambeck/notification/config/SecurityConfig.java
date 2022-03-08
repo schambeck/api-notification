b9d60bd4700aa59e0af6f5a48d19a8c8d5c90d8b
Marcelo Schambeck <mschambeck@gmail.com>
2022-03-08 04:42:25 -0300

    Spring Security

---
+++ src/main/java/com/schambeck/notification/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.schambeck.notification.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Profile("!test")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers(GET, "/notifications").authenticated()
+                .mvcMatchers(POST, "/notifications").authenticated()
+                .mvcMatchers(PUT, "/notifications").authenticated()
+                .anyRequest().permitAll()
+                .and().oauth2ResourceServer().jwt().decoder(jwtDecoder());
+    }
+
+    private JwtDecoder jwtDecoder() {
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withIssuer);
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
+    }
+
+}
